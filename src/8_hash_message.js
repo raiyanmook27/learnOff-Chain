870e2550d3f70cff7d0df64a879a41498d301b4f
raiyanmook27 <raiyan.mook27@gmail.com>
2023-06-18 10:56:53 +0000

    FEAT: HASH MESSAGE MODIFY

--- src/8_hash_message.js
+++ src/8_hash_message.js
@@ -3,5 +3,11 @@ const ethers = require("ethers");
 async function main() {
   const hashMessage = ethers.hashMessage("Hello World");
   console.log(hashMessage);
+
+  const getBytesMessage = ethers.id("Hello World");
+  console.log(getBytesMessage);
+
+  const keccak256Message = ethers.keccak256(getBytesMessage);
+  console.log(keccak256Message);
 }
 main();
